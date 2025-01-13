1a020424ad0d6a0c41ac38af68c709236cdff201
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2025-01-13 11:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,33 @@ function convertToRoman(num) {
 	return roman;
 
 }
+
+
+
+// function convertToRoman(num) {
+//     const obj = [
+//         ['M', 1000],
+//         ['D', 500],
+//         ['C', 100],
+//         ['L', 50],
+//         ['X', 10],
+//         ['V', 5],
+//         ['I', 1]
+//     ];
+
+//     let roman = '';
+//     for (const [symbol, value] of obj) {
+//         while (num >= value) {
+//             roman += symbol;
+//             num -= value;
+//         }
+//     }
+//     return roman;
+// }
+
+// // Example usage:
+// console.log(convertToRoman(1987)); // Output: MCMLXXXVII
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
